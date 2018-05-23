79872cd0f92006a52062131ce165318dd3f30986
Trobiun <robin@localhost>
2018-05-24 00:25:56 +0200

    Ajout de l'affichage de jours et des lignes entières de conversation

--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 DIR_LOGS="/var/lib/znc/users/trobiun/networks/twitch/moddata/log/#mygowd"			#le répertoire des fichiers de log
 EMOTES_FILE="listEmotes.txt"									#le fichier contenant les emotes à compter
+DAYS=$(ls -lq "$DIR_LOGS" | wc -l)
+ALL_LINES=$(grep -r "\*\*\*" "$DIR_LOGS" | wc -l)
+echo "Statistiques faites sur $DAYS jours et $ALL_LINES lignes :"
 EMOTES_GREPED=$(grep -o -h -w -i -r -f "$EMOTES_FILE" "$DIR_LOGS")				#récupère chaque utilisation de toutes les emotes
 TOTAL_WORDS=$(echo "$EMOTES_GREPED" | wc -l)							#compte le nombre total d'emotes utilisées
 COUNT_WORDS=$(echo "$EMOTES_GREPED" | sort -f | uniq -c -i | sed -e 's/^[[:space:]]*//')	#compte le nombre d'utilisation pour toutes les emotes
@@ -15,8 +18,9 @@ do
 	echo "	emote/total	= $EMOTES_PER_TOTAL %"						#affiche le pourcentage d'utilisation (en mots) de l'emote
 	echo "	lignes		= $COUNT_LINES	/ $TOTAL_LINES"					#affiche le nombre de lignes contenant l'emote actuelle et le total de lignes contenant une emote
 	WORDS_PER_LINE=$(bc -l <<< "scale=7; $WORDS / $COUNT_LINES")				#calcule le nombre d'emote utilisée par ligne
-	echo "	emotes/lignes	= $WORDS_PER_LINE"						#affiche le nombre d'emote utilisée par ligne
+	echo "	emotes/ligne	= $WORDS_PER_LINE"						#affiche le nombre d'emote utilisée par ligne
 done < "$EMOTES_FILE"
-SORTED=$(echo "$EMOTES_GREPED" | sort -f | uniq -c -i | sort -n | sed -e 's/^[[:space:]]*//' | awk '{ print $2	"	: " $1 }' )
-echo "Triées par nombre d'utilisation :"
+SORTED=$(echo "$EMOTES_GREPED" | sort -f | uniq -c -i | sort -n | sed -e 's/^[[:space:]]*//' | awk '{ print $2 ": " $1 }' )
+echo ""
+echo "Triées par utilisation :"
 echo "$SORTED"
