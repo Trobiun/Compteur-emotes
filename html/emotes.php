9405615fea9046490ce7a7165877f60ceda1236d
Trobiun <robin.tissot@free.fr>
2018-05-30 15:49:56 +0200

    ajout des paramètres sortyby et orde

--- html/emotes.php
+++ html/emotes.php
@@ -1,8 +1,15 @@
 <?php
 
-$sort = filter_input(INPUT_GET, 'sort', FILTER_SANITIZZE_FULL_SPECIAL_CHARSS);
+$sortby = escapeshellarg(filter_input(INPUT_GET,'sortby',FILTER_SANITIZE_FULL_SPECIAL_CHARS));
+if ($sortby == null) {
+	$sort = "false";
+}
 
-exec("./render.sh template.html emotesTemplate.sh $sort 2>&1",$array);
+$order = escapeshellarg(filter_input(INPUT_GET,'order',FILTER_SANITIZE_FULL_SPECIAL_CHARS));
+if ($order == null) {
+	$order = "asc";
+}
+exec("./render.sh template.html emotes_template.sh $sortby $order 2>&1",$array);
 foreach ($array as $key =>$value) {
 	print_r($value);
 }
