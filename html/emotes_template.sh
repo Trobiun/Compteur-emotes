ee809468baadc393b8395cb2a445bd232fc6aaec
Trobiun <robin.tissot@free.fr>
2018-05-30 15:44:13 +0200

    Corrections de bugs et ajout de l'ordre pour le tri numérique et alphabétique

--- html/emotes_template.sh
+++ html/emotes_template.sh
@@ -1,29 +1,68 @@
 #!/bin/bash
-dir_logs="/var/lib/znc/users/trobiun/networks/twitch/moddata/log/#mygowd"			#le répertoire des fichiers de log
-emotes_file="listEmotes.txt"									#le fichier contenant les emotes à compter
-days=$(ls -lq "$dir_logs" | wc -l)
-all_lines=$(grep -r "\*\*\*" "$dir_logs" | wc -l)
-emotes_greped=$(grep -o -h -w -i -r -f "$emotes_file" "$dir_logs")				#récupère chaque utilisation de toutes les emotes
-total_words=$(echo "$emotes_greped" | wc -l)							#compte le nombre total d'emotes utilisées
-count_words=$(echo "$emotes_greped" | sort -f | uniq -c -i | sed -e 's/^[[:space:]]*//')	#compte le nombre d'utilisation pour toutes les emotes
-total_lines=$(grep -w -i -r -f "$emotes_file" "$dir_logs" | wc -l)				#compte le nombre total de lignes contenant une emote
-sort="${@: -1}"
-emotes_while=$(cat "$emotes_file")								#définit les emotes qui seront parcourues par les emotes dans le fichier qui liste les emotes
-if [ "$sort" = true ]
+set -o errexit
+set -o nounset
+
+#fonctions
+mysort () {
+	sortArgs="--ignore-case"
+	if [ "${1}" = "numeric" ]
+	then
+		sortArgs="--numeric-sort"
+		sort_message="utilisation"
+	else
+		sortArgs="--dictionary-order ${sortArgs}"
+		sort_message="alphabétique"
+	fi
+	if [ "$order" = "dsc" ]
+	then
+		sortArgs="--reverse ${sortArgs}"
+		orderMessage="descendant"
+	else
+		orderMessage="ascendant"
+	fi
+	count_words="${@:3}"
+	if [ "${1}" = "numeric" ]
+	then
+		emotes_while=$(echo "${count_words}" | sort ${sortArgs} | awk '{ print $2 ":" $1 }')
+	else
+		emotes_while=$(echo "${count_words}" | awk '{ print $2 ":" $1 }' | sort ${sortArgs})
+	fi
+	echo "${emotes_while}"
+}
+
+#paramètres
+DIR_LOGS="/var/lib/znc/users/trobiun/networks/twitch/moddata/log/#mygowd"			#le répertoire des fichiers de log
+EMOTES_FILE="emotes_unsorted.list"									#le fichier contenant les emotes à compter
+#arguments provenant de l'appel du script php
+sortby="${@:3:1}"
+order="${@:4:1}"
+
+days=$(find "${DIR_LOGS}" -type f | wc --lines)
+all_lines=$(grep --recursive --invert-match "\*\*\*" "${DIR_LOGS}" | wc --lines)
+emotes_greped=$(grep --only-matching --no-filename --word-regexp --ignore-case --recursive --file="${EMOTES_FILE}" "${DIR_LOGS}")				#récupère chaque utilisation de toutes les emotes
+total_words=$(echo "$emotes_greped" | wc --lines)							#compte le nombre total d'emotes utilisées
+count_words=$(echo "$emotes_greped" | sort --ignore-case | uniq --count --ignore-case | sed --expression='s/^[[:space:]]*//')	#compte le nombre d'utilisation pour toutes les emotes
+emotes_while=$(mysort "$sortby" "$order" "${count_words[@]}")
+total_lines=$(grep --word-regexp --ignore-case --recursive --file="${EMOTES_FILE}" "${DIR_LOGS}" | wc --lines)				#compte le nombre total de lignes contenant une emote
+if [ "${order}" = "asc" ]
 then
-	sorted=$(echo "$emotes_greped" | sort -f | uniq -c -i | sort -n | awk '{ print $2 " : " $1 }' ) #trie les emotes par utilisation et les place au début de la ligne
-	emotes_while=$(echo "$sorted" | cut -d ":" -f 1)					#définit le emotes qui seront parcourues par les emotes triées par utilisation
-	sort_message="utilisation"
+	orderMessage="croissant"
 else
-	sort_message="ordre alphabétique"
+	orderMessage="décroissant"
+fi
+if [ "${sortby}" != "numeric" ]
+then
+	sort_message="ordre alphabétique ${orderMessage}"
+else
+	sort_message="ordre numérique ${orderMessage}"
 fi
 i=1
 while read -r emote;										#parcourt le fichier emotes_file
 do
-	emotes[$i]="$emote"
-	words[$i]=$(grep -i -w "$emote" <<< "$count_words" | cut -d " " -f 1)			#récupère le nombre d'utilisation (en mots) de l'emote actuelle
-	count_lines[$i]=$(grep -w -i -r "$emote" "$dir_logs" | wc -l)				#compte le nombre de lignes dans lesquelles l'emote apparaît
-	emotes_per_total[$i]=$(bc -l <<< "scale=7; (${words[$i]} / ${total_words}) * 100")	#calcule le poucentage d'utilisation (en mots) de l'emote
-	words_per_line[$i]=$(bc -l <<< "scale=7; ${words[$i]} / ${count_lines[$i]}")		#calcule le nombre d'emote utilisée par ligne
+	emotes[$i]=$(cut --delimiter=":" --fields=1 <<< "${emote}")
+	words[$i]=$(grep --ignore-case --word-regexp "${emotes[$i]}" <<< "${count_words}" | cut --delimiter=" " --fields=1)			#récupère le nombre d'utilisation (en mots) de l'emote actuelle
+	count_lines[$i]=$(grep --word-regexp --ignore-case --recursive "${emotes[$i]}" "${DIR_LOGS}" | wc --lines)				#compte le nombre de lignes dans lesquelles l'emote apparaît
+	emotes_per_total[$i]=$(bc --mathlib <<< "scale=7; (${words[$i]} / ${total_words}) * 100")	#calcule le poucentage d'utilisation (en mots) de l'emote
+	words_per_line[$i]=$(bc --mathlib <<< "scale=7; ${words[$i]} / ${count_lines[$i]}")		#calcule le nombre d'emote utilisée par ligne
 	((i++))
 done <<< "${emotes_while}"
